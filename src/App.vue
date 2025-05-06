7f1e6cc2e7a95e649014ace691a7d3ea060b0fb9
chinmaysah <chinmaysah@gmail.com>
2025-05-06 11:10:11 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Input from '@/assets/input/Input.vue';
+import Card from '@/components/Card.vue';
+import Table from '@/components/Table.vue';
+</script>
+
+<template>
+  <nav class="flex items-center justify-between h-16 px-6 border-b bg-muted/40">
+    <form class="w-full max-w-96">
+      <Input type="text" value="Search..." class="w-full pl-8 bg-background" />
+    
+    </form>
+    <div class="w-8 h-8 bg-white rounded-full"></div>
+
+  </nav>
+  <main>
+    <div class="relative">
+        <Card class="relative mt-10"></Card>
+      </div>
+      <Table></Table>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
